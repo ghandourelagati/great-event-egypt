052e3577166f450a08f9847261f12b6e1dc916b4
Ahmed Ghandour <ghandourelagati@gmail.com>
2021-01-10 12:47:00 +0200

    Next firebase/firestore

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,29 +1,85 @@
-import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
-import { connect } from 'react-redux'
-import { incrementCounter, decrementCounter } from './testActions'
+import React, { Component } from 'react';
+import { Button, Icon } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import GoogleMapReact from 'google-map-react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+import { incrementCounter, decrementCounter } from './testActions';
 
 const mapState = (state) => ({
-  data: state.test.data
-})
+  data: state.test.data,
+});
 
 const actions = {
   incrementCounter,
-  decrementCounter
-}
+  decrementCounter,
+};
+
+const Marker = () => <Icon name="marker" size="big" color="red" />;
 
 class TestComponent extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33,
+    },
+    zoom: 11,
+  };
+
+  state = {
+    address: '',
+  };
+
+  handleScriptLoad = () => {
+    this.setState({ scriptLoaded: true });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    geocodeByAddress(this.state.address)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => console.log('Success', latLng))
+      .catch((error) => console.error('Error', error));
+  };
+
+  onChange = (address) => this.setState({ address });
+
   render() {
-    const {incrementCounter, decrementCounter, data} = this.props;
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange,
+    };
+
+    const { incrementCounter, decrementCounter, data } = this.props;
     return (
       <div>
         <h1>Test Area</h1>
         <h3>The answer is: {data}</h3>
-        <Button onClick={incrementCounter} color='green' content='Increment' />
-        <Button onClick={decrementCounter} color='red' content='Decrement' />
+        <Button onClick={incrementCounter} color="green" content="Increment" />
+        <Button onClick={decrementCounter} color="red" content="Decrement" />
+        <br />
+        <br />
+        <form onSubmit={this.handleFormSubmit}>
+          <PlacesAutocomplete inputProps={inputProps} />
+          <button type="submit">Submit</button>
+        </form>
+
+        <div style={{ height: '300px', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: 'AIzaSyDMH9JnK0bH4R-Wv2TC9FHknDRbqJNNQwY',
+            }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}>
+            <Marker lat={59.955413} lng={30.337844} text={'Kreyser Avrora'} />
+          </GoogleMapReact>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default connect(mapState, actions)(TestComponent)
+export default connect(mapState, actions)(TestComponent);
